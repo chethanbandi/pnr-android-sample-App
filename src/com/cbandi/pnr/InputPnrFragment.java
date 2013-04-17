a51dd7289df1bbd0686f518882d9623e9099f3e8
Chethan Bandi <chethanbandi@gmail.com>
2013-04-17 22:41:22 +0530

    Now input PNR edit text reacts to keyboard action

--- src/com/cbandi/pnr/InputPnrFragment.java
+++ src/com/cbandi/pnr/InputPnrFragment.java
@@ -1,14 +1,70 @@
 package com.cbandi.pnr;
 
+import java.math.BigInteger;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class InputPnrFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.pnr_input, container, false);
 	}
+	
+	@Override
+	public void onViewCreated(final View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		
+		EditText editText = (EditText)view.findViewById(R.id.pnr_number);
+		
+		editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {	
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if(actionId == EditorInfo.IME_ACTION_GO) {
+					getPNR(view);
+				}
+				
+				return false;
+			}
+		});
+		
+		Button button = (Button)view.findViewById(R.id.get_status);
+		button.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getPNR(view);
+			}	
+		});
+	}
+	
+    public void getPNR(View view) {    	
+    	EditText pnrText = (EditText)view.findViewById(R.id.pnr_number);
+    	String pnrTmp = pnrText.getText().toString().trim();
+
+    	try {
+    		BigInteger pnr = new BigInteger(pnrTmp);
+    		
+        	if(pnrTmp.length() != 10) {
+        		Util.showAlert(getActivity(), getString(R.string.pnr_validation_length), false);
+    		} else {
+    			Intent intent = new Intent(getActivity(), PnrResultActivity.class);
+    			intent.putExtra(C.INTENT_KEY_PNR, pnr.toString());
+        	
+    			startActivity(intent);
+    		}
+    	} catch (Exception e) {
+    		Log.d(C.DEBUG_TAG, e.getMessage());
+    		Util.showAlert(getActivity(), getString(R.string.pnr_validation_non_numeric), false);
+    	}
+    }
 }
--- src/com/cbandi/pnr/MainActivity.java
+++ src/com/cbandi/pnr/MainActivity.java
@@ -1,43 +1,18 @@
 package com.cbandi.pnr;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends FragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
-    }
-    
-    public void getPNR(View view) {
-    	EditText pnrText = (EditText) findViewById(R.id.pnr_number);
-    	String pnrTmp = pnrText.getText().toString().trim();
-    	
-    	try {
-    		BigInteger pnr = new BigInteger(pnrTmp);
-    		
-        	if(pnrTmp.length() != 10) {
-        		Log.d(C.DEBUG_TAG, "invalid pnr number");
-        		Util.showAlert(this, getString(R.string.pnr_validation_length), false);
-    		} else {
-    			Intent intent = new Intent(this, PnrResultActivity.class);
-    			intent.putExtra(C.INTENT_KEY_PNR, pnr.toString());
-        	
-    			startActivity(intent);
-    		}
-    	} catch (Exception e) {
-    		Log.d(C.DEBUG_TAG, e.getMessage());
-    		Util.showAlert(this, getString(R.string.pnr_validation_non_numeric), false);
-    	}
     }
     
     public void scanSms(View view) {
